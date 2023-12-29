eef76f7e70f90b34ea1d36ba857f986965d46a9b
Filip Pawłowicz <pawlowicz.filip137@gmail.com>
2023-12-29 09:37:54 +0100

    created MapDirection enum

---
+++ DarwinWorld/src/main/java/MapDirection.java
@@ -0,0 +1,11 @@
+public enum MapDirection {
+    //
+    NORTH(0), NORTH_EAST(1), EAST(2), EAST_SOUTH(3);
+
+    private final int turn;
+    MapDirection (int turn) {
+        this.turn = turn;
+    }
+
+    public
+}
