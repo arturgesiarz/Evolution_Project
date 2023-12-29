3daa558c9c042f620df28351cc6c6cd3e1aac340
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-12-29 09:42:51 +0100

    Created Vector2d

---
+++ DarwinWorld/src/main/java/Vector2d.java
@@ -0,0 +1,18 @@
+public class Vector2d {
+    private final int x;
+    private final int y;
+    public Vector2d(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    @Override
+    public String toString(){
+        return "(%d,%d)".formatted(this.x,this.y);
+    }
+}
