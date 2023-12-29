33c7436daa4c2fe82ec80b77776e79f7057ddc26
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2023-12-29 11:50:40 +0100

    Created DarwinApp

--- DarwinWorld/src/main/java/oop/presenter/DarwinPresenter.java
+++ DarwinWorld/src/main/java/oop/presenter/DarwinPresenter.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class DarwinPresenter {
+    private static final double CELL_WIDTH = 30; //TODO ZMIENIC stala sluzaca do okreslenia szerokosci okienka
+    private static final double CELL_HEIGHT = 30; //TODO ZMIENIC stala sluzaca do okreslenia wysokosci okienka
     @FXML
     public Label infoLabel;
     @FXML
