4f550f1ac0c7e5f8bb1dfc4ca946ca46ff9b5188
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-17 16:43:39 +0100

    Some changes

--- DarwinWorld/src/main/java/oop/DarwinGUI.java
+++ DarwinWorld/src/main/java/oop/DarwinGUI.java
@@ -14,10 +14,5 @@ import java.util.List;
 public class DarwinGUI {
     public static void main(String[] args){
         // Application.launch(DarwinApp.class, args);
-        List<Vector2d> positions = List.of(new Vector2d(0,2), new Vector2d(0,0));  // tworze przykladowe pozycje poczatkowe naszych zwierzat
-        List<GenesHandler> genes = List.of(new GenesBasic(List.of(1,2,3,4,5,6)), new GenesBasic(List.of(0,2,3,5,6,1)));
-        RectangularMap map = new RectangularMap(10, 10, new MapParameters(5,2));
-        Simulation simulation = new Simulation(positions, genes, map);
-
     }
 }
