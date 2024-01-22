a362c474d22fa4bfd6dd2ba89e86156d4183ae72
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-22 14:29:12 +0100

    Qs

--- DarwinWorld/src/main/java/oop/presenter/DarwinPresenter.java
+++ DarwinWorld/src/main/java/oop/presenter/DarwinPresenter.java
@@ -3,9 +3,7 @@ package oop.presenter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
@@ -15,7 +13,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import oop.model.util.MapParameters;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DarwinPresenter {
     private static final double CELL_WIDTH = 30;
@@ -38,7 +39,7 @@ public class DarwinPresenter {
     @FXML
     public TextField energyOnePlant;
     @FXML
-    public TextField energyAnimalBeggining;
+    public TextField energyAnimalBeginning;
     @FXML
     public TextField energyFullAnimal;
     @FXML
@@ -55,11 +56,14 @@ public class DarwinPresenter {
     public TextField maximumMutation;
     @FXML
     public TextField genomeLength;
+    @FXML
+    public ComboBox saveComboBox;
 
     @FXML
     private ComboBox<String> configurationsComboBox;
     private MapParameters mapParameters;
-
+    private int mapModeOn = -1;
+    private int genesModeOn = -1;
 
     @FXML
     private void initialize() {
@@ -74,37 +78,51 @@ public class DarwinPresenter {
             }
             return null;
         }));
-
-
-
-
-
+        mapHeight.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), null, change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d{0,3}")) {
+                return change;
+            }
+            return null;
+        }));
+        energyOnePlant.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), null, change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d{0,2}")) {
+                return change;
+            }
+            return null;
+        }));
     }
 
     @FXML
     public void onSimulationStartClicked(ActionEvent actionEvent) {
+
     }
     @FXML
     public void onMapBasic() {
         mapBasicToggleButton.setDisable(true);
         mapExtendedToggleButton.setDisable(false);
+        mapModeOn = 1;
     }
     @FXML
     public void onMapExtended() {
         mapBasicToggleButton.setDisable(false);
         mapExtendedToggleButton.setDisable(true);
+        mapModeOn = 0;
     }
 
     @FXML
     public void onGenesBasic() {
         genesBasicToggleButton.setDisable(true);
         genesExtendedToggleButton.setDisable(false);
+        genesModeOn = 1;
     }
 
     @FXML
     public void onGenesExtended() {
         genesBasicToggleButton.setDisable(false);
         genesExtendedToggleButton.setDisable(true);
+        genesModeOn = 0;
 
     }
 
@@ -131,6 +149,48 @@ public class DarwinPresenter {
         else{
             showFileReadErrorAlert();
         }
+    }
+
+    @FXML
+    public void onSaveFileClicked() {
+
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JSON files (*.json)", "*.json");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showSaveDialog(null);
+
+        if (file != null) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, Object> dataToSave = saveData();
+                objectMapper.writeValue(file, dataToSave);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Map<String, Object> saveData(){
+        Map<String, Object> dataToSave = new HashMap<>();
+
+        dataToSave.put("width", mapWidth.getText());
+        dataToSave.put("height", mapHeight.getText());
+        dataToSave.put("mapMode", String.valueOf(mapModeOn));
+        dataToSave.put("amountOfPlantsBeginning", plantsBeginning.getText());
+        dataToSave.put("grassEnergy", energyOnePlant.getText());
+        dataToSave.put("amountOfPlantsDaily", plantsPerDay.getText());
+        dataToSave.put("initialNumberOfAnimals", animalsBeginning.getText());
+        dataToSave.put("startEnergy", energyAnimalBeginning.getText());
+        dataToSave.put("minimumEnergyRequiredForCopulation", energyFullAnimal.getText());
+        dataToSave.put("energyLostInCopulation",energyLoseForBaby.getText());
+        dataToSave.put("minimumNumberOfMutation", minumumMutation.getText());
+        dataToSave.put("maximumNumberOfMutation", maximumMutation.getText());
+        dataToSave.put("genesMode", String.valueOf(genesModeOn));
+        dataToSave.put("genomeLength", genomeLength.getText());
+
+        return dataToSave;
     }
 
     private void showFileReadErrorAlert() {
@@ -176,7 +236,7 @@ public class DarwinPresenter {
 
         // konfiguracja energii
         energyOnePlant.setText(String.valueOf(mapParameters.grassEnergy()));
-        energyAnimalBeggining.setText(String.valueOf(mapParameters.startEnergy()));
+        energyAnimalBeginning.setText(String.valueOf(mapParameters.startEnergy()));
         energyFullAnimal.setText(String.valueOf(mapParameters.minimumEnergyRequiredForCopulation()));
         energyLoseForBaby.setText(String.valueOf(mapParameters.energyLostInCopulation()));
 
@@ -196,7 +256,6 @@ public class DarwinPresenter {
         }
 
     }
-
 
 
 }
